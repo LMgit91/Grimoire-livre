22b17a3e1e3e56eee0634dbde105adb75fbd797a
LMgit91 <163318506+LMgit91@users.noreply.github.com>
2024-10-01 22:55:47 +0200

    Delete routes directory

--- routes/userInformation.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../midleware/auth');
-const userCtrl = require('../controller/userInformation');
-
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
-
-module.exports = router;
