e02fbda93c5a46f7c209db987f7d447414398ecb
MohaD <DMRgit@outlook.fr>
2025-05-01 18:05:27 +0200

    ajout book

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const bookRoad = require('./routes/book');
+const bookRoad = require('./routes/Book');
 const userRoad = require('./routes/userInformation');
 const path = require('path');
 
