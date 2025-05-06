67752b577c1b3b5e2f19065d5417f98d7165674b
MohaD <DMRgit@outlook.fr>
2025-05-06 21:07:07 +0200

    Updates

---
+++ precache-manifest.1d19870c1f9273dcbf17de41efe4f211.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc103259dfce90ad503ba8180bd99894",
+    "url": "/Grimoire-livre/index.html"
+  },
+  {
+    "revision": "59dec96dc0651286ce7e",
+    "url": "/Grimoire-livre/static/css/main.1ae55a85.chunk.css"
+  },
+  {
+    "revision": "c26df88c1ee05b158f04",
+    "url": "/Grimoire-livre/static/js/2.64774d8f.chunk.js"
+  },
+  {
+    "revision": "16b776abcf61877089b4aed4b38b9c54",
+    "url": "/Grimoire-livre/static/js/2.64774d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8944fead1be870f5cb",
+    "url": "/Grimoire-livre/static/js/3.b94a50de.chunk.js"
+  },
+  {
+    "revision": "59dec96dc0651286ce7e",
+    "url": "/Grimoire-livre/static/js/main.9871d673.chunk.js"
+  },
+  {
+    "revision": "18fe02cf27fe9326c4bc",
+    "url": "/Grimoire-livre/static/js/runtime-main.d2905431.js"
+  },
+  {
+    "revision": "92701564814c392ae5ab17f60deac488",
+    "url": "/Grimoire-livre/static/media/Logo.92701564.svg"
+  },
+  {
+    "revision": "aad45f62924a37afeacd3b82b638e338",
+    "url": "/Grimoire-livre/static/media/book_add.aad45f62.jpg"
+  },
+  {
+    "revision": "6b4ebe26cc68423071f1ea801b2ab676",
+    "url": "/Grimoire-livre/static/media/book_delete.6b4ebe26.png"
+  },
+  {
+    "revision": "95688b52d487025f394dee4f3899fb5a",
+    "url": "/Grimoire-livre/static/media/home_banner.95688b52.jpg"
+  },
+  {
+    "revision": "6630600ae2b072fd1163a3634cdb5335",
+    "url": "/Grimoire-livre/static/media/map_footer.6630600a.png"
+  }
+]);
